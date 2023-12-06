85753ac1d1e1979521874a6a240e8b4137bae352
Sascha-davidson <saschadavidson1@gmail.com>
2023-12-06 17:13:17 +0100

    beginnen for first code challange
    ik hoef alleen nog de javascript te maken

---
+++ Ridiculous-maximalist-cursor-effect/script.js
@@ -0,0 +1,8 @@
+let cursor = document.querySelector("#cursor");
+let frisbee = document.querySelector("#frisbee")
+let container = document.querySelector("#container")
+
+// 1. cursor follows real cursor
+// 2. frisbee follows cursor but delayed
+// 3. both may not go outside container
+// 4. cursor rotate towards real cursor
