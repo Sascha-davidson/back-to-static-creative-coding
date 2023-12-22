ca7595d4b5dd9f39e36d2078cf863545768ebb7c
Sascha-davidson <saschadavidson1@gmail.com>
2023-12-22 09:45:44 +0100

    small changes

--- Bizarre brutalist expanse/script.js
+++ Bizarre brutalist expanse/script.js
@@ -1,6 +1,6 @@
 /** @type {HTMLCanvasElement} */
 const nav = document.querySelector(".container");
-const navHeight = nav.offsetHeight
+const navHeight = nav.offsetHeight;
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
@@ -9,7 +9,6 @@ canvas.height = window.innerHeight - navHeight;
 canvas.style.height = canvas.height + "px";
 const canvasBounds = canvas.getBoundingClientRect();
 
-
 class Root {
   constructor(x, y) {
     this.x = x;
@@ -17,18 +16,22 @@ class Root {
     this.speedX = Math.random() * 4 - 2;
     this.speedY = Math.random() * 4 - 2;
     this.maxSize = Math.random() * 7 + 5;
-    this.size = Math.random() * 1 + 2;
+    this.size = Math.random() * 1 + 3;
+    this.lightness = 10;
   }
 
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
     this.size += 0.1;
+    if (this.lightness < 50) this.lightness += 0.25;
     if (this.size < this.maxSize) {
       ctx.beginPath();
+      // ctx.rect(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fill();
-      ctx.fillStyle = "#313131";
+      ctx.fillStyle = "hsl(0,0%," + this.lightness + "%)";
+
       ctx.stroke();
       ctx.strokeStyle = "#1E1E1E";
       requestAnimationFrame(this.update.bind(this));
@@ -37,16 +40,18 @@ class Root {
   }
 }
 
-document.addEventListener('mousemove', (e)=>{
+document.addEventListener("mousemove", (e) => {
   const isOverCanvas = isInBoundingBox(e.pageX, e.pageY, canvasBounds);
-  if(isOverCanvas === false) return;
+  if (isOverCanvas === false) return;
   const root = new Root(e.pageX, e.pageY);
   root.update();
-})
+});
 
-function isInBoundingBox(mouseX, mouseY, canvasBounds){
-  const isInHorizontal = mouseX >= canvasBounds.x && mouseX <= canvasBounds.width;
-  const isInVertical = mouseY >= canvasBounds.y && mouseY <= canvasBounds.height;
+function isInBoundingBox(mouseX, mouseY, canvasBounds) {
+  const isInHorizontal =
+    mouseX >= canvasBounds.x && mouseX <= canvasBounds.width;
+  const isInVertical =
+    mouseY >= canvasBounds.y && mouseY <= canvasBounds.height;
 
   return isInHorizontal && isInVertical;
 }
